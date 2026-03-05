cd31a49b2f72abda5e73fe133550766d0bb6338d
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-03-05 11:39:20 -0500

    fix: adjust open_col and close_col when va includes surrounding whitespace

--- lua/surround/init.lua
+++ lua/surround/init.lua
@@ -28,21 +28,9 @@ local notify = function(level, msg, ...)
   vim.notify(msg:format(...), level)
 end
 
---- @param char string
-local function trigger_visual(char)
-  local saved_visual_hl = vim.api.nvim_get_hl(0, { name = "Visual", })
-  local saved_cursor = vim.api.nvim_win_get_cursor(0)
-
-  vim.api.nvim_set_hl(0, "Visual", { link = "Normal", })
-  vim.cmd("normal! va" .. char)
-  vim.cmd "normal! \x1b"
-  vim.api.nvim_set_hl(0, "Visual", saved_visual_hl)
-  vim.api.nvim_win_set_cursor(0, saved_cursor)
-end
-
 --- @param start_mark string
 --- @param end_mark string
-local function get_pos_from_marks_0i(start_mark, end_mark)
+local function get_pos_from_marks(start_mark, end_mark)
   local open_pos = vim.api.nvim_buf_get_mark(0, start_mark)
   if open_pos[1] == 0 and open_pos[2] == 0 then return nil end
 
@@ -51,13 +39,15 @@ local function get_pos_from_marks_0i(start_mark, end_mark)
 
   if open_pos[1] == close_pos[1] and open_pos[2] == close_pos[2] then return nil end
 
-  local one_idx_offset = 1
-
   return {
-    open_row = open_pos[1] - one_idx_offset,
-    open_col = open_pos[2],
-    close_row = close_pos[1] - one_idx_offset,
-    close_col = close_pos[2],
+    open_row_0i = open_pos[1] - 1,
+    open_row_1i = open_pos[1],
+    open_col_0i = open_pos[2],
+    open_col_1i = open_pos[2] + 1,
+    close_row_0i = close_pos[1] - 1,
+    close_row_1i = close_pos[1],
+    close_col_0i = close_pos[2],
+    close_col_1i = close_pos[2] + 1,
   }
 end
 
@@ -69,14 +59,57 @@ local function clamp_to_line_len(row, col)
   return math.min(col, #line_text - one_idx_offset)
 end
 
+--- adjusts for whitespace that `va` may include for quote text objects.
+--- @param char string
+--- @return table|nil
+local function find_surround_pos(char)
+  local saved_visual_hl = vim.api.nvim_get_hl(0, { name = "Visual", })
+  local saved_cursor = vim.api.nvim_win_get_cursor(0)
+
+  vim.api.nvim_set_hl(0, "Visual", { link = "Normal", })
+  vim.cmd("normal! va" .. char)
+  vim.cmd "normal! \x1b"
+  vim.api.nvim_set_hl(0, "Visual", saved_visual_hl)
+  vim.api.nvim_win_set_cursor(0, saved_cursor)
+
+  local pos = get_pos_from_marks("<", ">")
+  if pos == nil then return nil end
+
+  local pair = get_pair(char)
+  if pair == nil then return nil end
+
+  local open_line = vim.api.nvim_buf_get_lines(0, pos.open_row_0i, pos.open_row_0i + 1, true)[1]
+  if open_line:sub(pos.open_col_1i, pos.open_col_1i) ~= pair.open then
+    for i = pos.open_col_1i + 1, #open_line do
+      if open_line:sub(i, i) == pair.open then
+        pos.open_col_0i = i - 1
+        pos.open_col_1i = i
+        break
+      end
+    end
+  end
+
+  local close_line = vim.api.nvim_buf_get_lines(0, pos.close_row_0i, pos.close_row_0i + 1, true)[1]
+  if close_line:sub(pos.close_col_1i, pos.close_col_1i) ~= pair.close then
+    for i = pos.close_col_0i, 1, -1 do
+      if close_line:sub(i, i) == pair.close then
+        pos.close_col_0i = i - 1
+        pos.close_col_1i = i
+        break
+      end
+    end
+  end
+
+  return pos
+end
+
 
 M.setup = function()
   vim.keymap.set("n", "ds", function()
     local char = vim.fn.nr2char(vim.fn.getchar())
 
     _G.__surround_delete = function()
-      trigger_visual(char)
-      local pair_pos = get_pos_from_marks_0i("<", ">")
+      local pair_pos = find_surround_pos(char)
 
       if pair_pos == nil then
         notify(vim.log.levels.ERROR, "No matching pair")
@@ -84,11 +117,11 @@ M.setup = function()
       end
 
       vim.api.nvim_buf_set_text(0,
-        pair_pos.close_row, pair_pos.close_col, pair_pos.close_row, pair_pos.close_col + 1,
+        pair_pos.close_row_0i, pair_pos.close_col_0i, pair_pos.close_row_0i, pair_pos.close_col_0i + 1,
         { "", }
       )
       vim.api.nvim_buf_set_text(0,
-        pair_pos.open_row, pair_pos.open_col, pair_pos.open_row, pair_pos.open_col + 1,
+        pair_pos.open_row_0i, pair_pos.open_col_0i, pair_pos.open_row_0i, pair_pos.open_col_0i + 1,
         { "", }
       )
     end
@@ -112,8 +145,7 @@ M.setup = function()
         end
       end
 
-      trigger_visual(old_char)
-      local old_pair_pos = get_pos_from_marks_0i("<", ">")
+      local old_pair_pos = find_surround_pos(old_char)
 
       if old_pair_pos == nil then
         notify(vim.log.levels.ERROR, "No matching pair")
@@ -121,11 +153,11 @@ M.setup = function()
       end
 
       vim.api.nvim_buf_set_text(0,
-        old_pair_pos.close_row, old_pair_pos.close_col, old_pair_pos.close_row, old_pair_pos.close_col + 1,
+        old_pair_pos.close_row_0i, old_pair_pos.close_col_0i, old_pair_pos.close_row_0i, old_pair_pos.close_col_0i + 1,
         { new_pair_cached.close, }
       )
       vim.api.nvim_buf_set_text(0,
-        old_pair_pos.open_row, old_pair_pos.open_col, old_pair_pos.open_row, old_pair_pos.open_col + 1,
+        old_pair_pos.open_row_0i, old_pair_pos.open_col_0i, old_pair_pos.open_row_0i, old_pair_pos.open_col_0i + 1,
         { new_pair_cached.open, }
       )
     end
@@ -147,13 +179,14 @@ M.setup = function()
         end
       end
 
-      local pos = get_pos_from_marks_0i("[", "]")
+      local pos = get_pos_from_marks("[", "]")
       assert(pos ~= nil)
-      pos.close_col = clamp_to_line_len(pos.close_row, pos.close_col)
+      pos.close_col_0i = clamp_to_line_len(pos.close_row_0i, pos.close_col_0i)
 
-      vim.api.nvim_buf_set_text(0, pos.close_row, pos.close_col + 1, pos.close_row, pos.close_col + 1,
+      vim.api.nvim_buf_set_text(0, pos.close_row_0i, pos.close_col_0i + 1, pos.close_row_0i, pos.close_col_0i + 1,
         { pair_cached.close, })
-      vim.api.nvim_buf_set_text(0, pos.open_row, pos.open_col, pos.open_row, pos.open_col, { pair_cached.open, })
+      vim.api.nvim_buf_set_text(0, pos.open_row_0i, pos.open_col_0i, pos.open_row_0i, pos.open_col_0i,
+        { pair_cached.open, })
     end
 
     vim.o.operatorfunc = "v:lua.__surround_add"
@@ -169,12 +202,13 @@ M.setup = function()
     end
 
     vim.cmd "normal! \x1b"
-    local pos = get_pos_from_marks_0i("<", ">")
+    local pos = get_pos_from_marks("<", ">")
     assert(pos ~= nil)
-    pos.close_col = clamp_to_line_len(pos.close_row, pos.close_col)
+    pos.close_col_0i = clamp_to_line_len(pos.close_row_0i, pos.close_col_0i)
 
-    vim.api.nvim_buf_set_text(0, pos.close_row, pos.close_col + 1, pos.close_row, pos.close_col + 1, { pair.close, })
-    vim.api.nvim_buf_set_text(0, pos.open_row, pos.open_col, pos.open_row, pos.open_col, { pair.open, })
+    vim.api.nvim_buf_set_text(0, pos.close_row_0i, pos.close_col_0i + 1, pos.close_row_0i, pos.close_col_0i + 1,
+      { pair.close, })
+    vim.api.nvim_buf_set_text(0, pos.open_row_0i, pos.open_col_0i, pos.open_row_0i, pos.open_col_0i, { pair.open, })
   end)
 end
 
